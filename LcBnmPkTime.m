3b8632cf1c1402752009617a6538bb7e638d5496
Bmunn <bmun7133@uni.sydney.edu.au>
2023-06-30 09:06:09 +1000

    Update LcBnmPkTime.m

--- LcBnmPkTime.m
+++ LcBnmPkTime.m
@@ -1,13 +1,13 @@
-function [LClocs,BNMlocs,LCBNMlocs,Baselocs] = LcBnmPkTime(lc_ts,bnm_ts,dt)
+function [LClocs,BNMlocs,Baselocs] = LcBnmPkTime(lc_ts,bnm_ts,dt)
 
 
-%Takes in the LC and BNM activity, finds the peaks times that satisfy the
-%phasic increase followed by significant peak 
+%Takes in the LC and BNM activity, and finds the peaks times that satisfy the
+%phasic increase followed by a significant peak 
 %ouputs the lc peaks, bnm peaks, LC+BNM and times outside of these windows
 
 %% For example process we have concatenated all recordings
-%Entries near end/beginning of subject within a dt (but factoring in acc
-%peak occuring 10 tr post must be atleast 10 TR)
+%Entries near the end/beginning of the subject within a dt (but factoring in acc
+%peak occurring 10 tr post must be at least 10 TR)
 
 edgvals = repmat(1050-max([dt 10]):1050+max([dt 10]),61,1) + repmat(-1050:1050:63000-1050,2*max([dt 10])+1,1)';
 edgvals = edgvals(:);
@@ -47,23 +47,6 @@ locs = locs(~ismember(locs,edgvals));
 locs = locs(sig(locs+10)>2*std(sig));
 
 BNMlocs = locs;
-
-%% LC+BNM peaks
-
-sig = bnm_ts+lc_ts; %grab signal
-
-
-%Calc acc
-sigAcc = gradient(gradient(sig));
-[~,locs] =  findpeaks(sigAcc,'MinPeakDistance',10); %find peaks of acc
-
-%Remove locs near boundaries
-locs = locs(~ismember(locs,edgvals));
-
-%Now only take the locations where the phasic burst (acc) maximises high later on 
-locs = locs(sig(locs+10)>2*std(sig));
-
-LCBNMlocs = locs;
 
 %% Baseline times
 
